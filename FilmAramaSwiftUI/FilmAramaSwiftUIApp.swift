15c43331bb0e8697c3c482ca8d43bb30a8bc744c
Atil Samancioglu <samancioglu.atil@gmail.com>
2020-10-11 16:11:00 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by Atil Samancioglu on 11.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
